185d00eb84e116c9f1e883678a1e04d09c6a393a
raverrr <divadbate@googlemail.com>
2021-01-19 16:19:34 +0000

    Add files via upload

---
+++ formfinder.js
@@ -0,0 +1,42 @@
+// FormFinder Script by @divadbate
+
+
+function scan(ps, msg, src) {
+    // first lets set up some details incase we find an email, these will populate the alert later
+    var alertRisk = 2
+    var alertReliability = 3
+    var alertTitle = 'Formfinder (script)'
+    var alertDesc = 'Forms were found'
+    var alertSolution = ''
+    var cweId = 0
+    var wascId = 0
+
+	//regex
+    var re = /<(form|input)>|([Mm]ethod)="(...|....|.....|......)"|(action)="/g
+
+	// we need to set the url variable to the request or we cant track the alert later
+    var url = msg.getRequestHeader().getURI().toString();
+
+	// exclude certain content types
+    var contenttype = msg.getResponseHeader().getHeader("Content-Type")
+	var unwantedfiletypes = ['image/png', 'image/jpeg','image/gif','application/x-shockwave-flash','application/pdf','application/javascript']
+	
+	if (unwantedfiletypes.indexOf(""+contenttype) >= 0) {
+		// if we find one of the unwanted headers quit this scan
+    		return
+	}else{
+	//regex against the body response
+        var body = msg.getResponseBody().toString()
+        if (re.test(body)) {
+            re.lastIndex = 0 
+          
+            var foundForm = []
+            var comm
+            while (comm = re.exec(body)) {
+                foundForm.push(comm[0]);
+            }
+		  // form found
+            ps.raiseAlert(alertRisk, alertReliability, alertTitle, alertDesc, url, '', '', foundForm.toString(), alertSolution, '', cweId, wascId, msg);
+        }
+    }
+}
